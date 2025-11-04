23424c049f0e26a4720f56f3dc3687fa310f0591
47332 <47332@students.d94.org>
2025-11-04 14:40:21 +0000

    js added for mobile hamburger

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-// Add JS here
+//Header Hamburger Open and Close
+const hamOpen = document.getElementById("hamburgerOpen");
+const hamClose = document.getElementById("hamburgerClose");
+const hamMenucontainer = document.getElementById('hamburgerMenuContainer');
+const hamMenuOverlay = document.getElementById('hamburgerMenuOverlay');
+
+hamOpen.addEventListener("click", () => {
+    hamMenucontainer.style.display = "flex";
+    hamMenuOverlay.style.display = "block";
+
+    hamOpen.classList.add("hamburgerActive");
+    hamClose.classList.add("hamburgerCloseActive");
+});
+
+hamClose.addEventListener("click", () => {
+    hamMenucontainer.style.display = "none";
+    hamMenuOverlay.style.display = "none";
+
+    hamOpen.classList.remove("hamburgerActive");
+    hamClose.classList.remove("hamburgerCloseActive");
+});
+
+hamMenuOverlay.addEventListener("click", () => {
+    hamMenucontainer.style.display = "none";
+    hamMenuOverlay.style.display = "none";
+  
+    hamOpen.classList.remove("hamburgerActive");
+    hamClose.classList.remove("hamburgerCloseActive");
+  });
